1d49d71bd3bd065d3f6e1799b9b77e14231ab216
Daniel <danchan020@gmail.com>
2022-09-23 18:34:25 -0700

    coin css

--- components/CryptoCoins.js
+++ components/CryptoCoins.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import styles from "../styles/CryptoCoins.module.css";
 
 export default function CryptoCoins() {
   return (
-    <div>CryptoCoins</div>
+    <div className={styles.all_coins}>
+        <h1>CryptoCoins</h1>
+    </div>
   )
 }
