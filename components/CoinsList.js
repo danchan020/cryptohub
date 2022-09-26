2a208293b3d476d590d6e3ddaf6db42a40188942
Daniel <danchan020@gmail.com>
2022-09-25 21:19:21 -0700

    api props

--- components/CoinsList.js
+++ components/CoinsList.js
@@ -7,7 +7,17 @@ export default function CoinsList({filteredCoins}) {
             return (
                 <Coin 
                 key = {coin.id}
-                
+                name = {coin.name}
+                symbol = {coin.symbol}
+                image = {coin.image}
+                price = {coin.current_price}
+                onehr = {coin.price_change_percentage_1h_in_currency}
+                twentyfourhr = {coin.price_change_percentage_24h_in_currency}
+                sevenday = {coin.price_change_percentage_7d_in_currency}
+                marketcap = {coin.market_cap}
+                volume = {coin.total_volume}
+                supply = {coin.circulating_supply}
+                trendline = {coin.sparkline_in_7d}
                 />
             )
         })}
