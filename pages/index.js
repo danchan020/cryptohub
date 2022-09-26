973ca89d6189c01d75dbc700e1eac4e436bcdffc
Daniel <danchan020@gmail.com>
2022-09-25 19:40:41 -0700

    api

--- pages/index.js
+++ pages/index.js
@@ -22,3 +22,12 @@ export default function Home() {
     </div>
   )
 }
+export const getServerSideProps = async () => {
+  const res = await fetch('https://coingecko.p.rapidapi.com/coins/markets?vs_currency=usd&price_change_percentage=1h%2C%2024h%2C%207d&page=1&sparkline=true&per_page=10&order=market_cap_desc')
+  const filteredCoins = await res.json()
+    return {
+      props: {
+        filteredCoins
+      }
+    }
+}
