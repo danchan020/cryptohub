f05c0e3216e1615431f899a005e4652ba045335e
Daniel <danchan020@gmail.com>
2022-09-26 19:26:38 -0700

    props

--- components/Coin.js
+++ components/Coin.js
@@ -1,9 +1,9 @@
 import styles from "../styles/Coin.module.css";
 
-export default function Coin() {
+export default function Coin({...props}) {
   return (
     <div className={styles.all_coins}>
-        <h1>CryptoCoins</h1>
+        <h1>{props.name}</h1>
     </div>
   )
 }
