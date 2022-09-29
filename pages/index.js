00ea1175ac1dd53596a7c0ce604f219a5866cf2d
Daniel <danchan020@gmail.com>
2022-09-28 22:04:25 -0700

    more

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home({filteredCoins}) {
 }
 export async function getServerSideProps(){
 
-  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d')
+  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d')
   const filteredCoins = await res.json()
     return {
       props: {filteredCoins}
