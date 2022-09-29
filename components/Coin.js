87db9ba4e2042419f5f3cddd053e724c6fef69af
Daniel <danchan020@gmail.com>
2022-09-28 22:21:07 -0700

    prop

--- components/Coin.js
+++ components/Coin.js
@@ -6,9 +6,18 @@ export default function Coin({...props}) {
       <div className={styles.coin_image}>
         <img className={styles.coin_image} src={props.image} ></img>
       </div>
-        
+      <div>
         <h1>{props.name}</h1>
         <p>{props.symbol}</p>
+        <p>{props.price}</p>
+        <p>{props.onehr}</p>
+        <p>{props.twentyfourhr}</p>
+        <p>{props.sevenday}</p>
+        <p>{props.marketcap}</p>
+        <p>{props.volume}</p>
+        <p>{props.supply}</p>
+      
+      </div>
     </div>
   )
 }
