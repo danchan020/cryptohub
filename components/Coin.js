39d82ad60ea83a598239ef9c690252a227e41276
Daniel <danchan020@gmail.com>
2022-09-28 21:43:24 -0700

    image size

--- components/Coin.js
+++ components/Coin.js
@@ -3,7 +3,10 @@ import styles from "../styles/Coin.module.css";
 export default function Coin({...props}) {
   return (
     <div className={styles.all_coins}>
-        <img src={props.image}></img>
+      <div className={styles.coin_image}>
+        <img className={styles.coin_image} src={props.image} ></img>
+      </div>
+        
         <h1>{props.name}</h1>
         <p>{props.symbol}</p>
     </div>
