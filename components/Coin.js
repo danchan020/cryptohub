7fb21b3a05d2f262fe2526a931603c137606c28f
Daniel <danchan020@gmail.com>
2022-09-29 00:10:46 -0700

    set

--- components/Coin.js
+++ components/Coin.js
@@ -2,22 +2,22 @@ import styles from "../styles/Coin.module.css";
 
 export default function Coin({...props}) {
   return (
-    <div className={styles.all_coins}>
+    <div className={styles.coin_row}>
       <div className={styles.coin_image}>
         <img className={styles.coin_image} src={props.image} ></img>
       </div>
       <div>
         <h1>{props.name}</h1>
-        <p>{props.symbol}</p>
-        <p>{props.price}</p>
+        <p>{props.symbol.toUpperCase()}</p>
+        <p>${props.price}</p>
         <p>{props.onehr}</p>
         <p>{props.twentyfourhr}</p>
         <p>{props.sevenday}</p>
         <p>{props.marketcap}</p>
         <p>{props.volume}</p>
         <p>{props.supply}</p>
-      
       </div>
     </div>
   )
 }
+//sparkline chart.js 
