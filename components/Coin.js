59f5e1f770f8ceea65dd7f75f5ce6beee8fe48c8
Daniel <danchan020@gmail.com>
2022-10-07 12:15:17 -0700

    works

--- components/Coin.js
+++ components/Coin.js
@@ -9,7 +9,7 @@ export default function Coin({...props}) {
         <p>{props.symbol.toUpperCase()}</p>
       </div>
       <div className={styles.coin_data}>
-        <p>${props.price}</p>
+        <p>${props.price.toLocaleString()}</p>
         {props.onehr < 0 ? (<p className={styles.coin_percent_red}>{props.onehr.toFixed(2)}%</p>) : (<p className={styles.coin_percent_green}>{props.onehr.toFixed(2)}%</p>)}
         {props.twentyfourhr < 0 ? (<p className={styles.coin_percent_red}>{props.twentyfourhr.toFixed(2)}%</p>) : (<p className={styles.coin_percent_green}>{props.twentyfourhr.toFixed(2)}%</p>)}
         {props.sevenday < 0 ? (<p className={styles.coin_percent_red}>{props.sevenday.toFixed(2)}%</p>) : (<p className={styles.coin_percent_green}>{props.sevenday.toFixed(2)}%</p>)}
